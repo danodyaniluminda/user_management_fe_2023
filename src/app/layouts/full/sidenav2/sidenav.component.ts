8230445709e865ceae93be1a11c0ccdd09686f59
Janith <wajwi@ou.ac.lk>
2023-06-09 13:56:22 +0530

    side nav connected with backend

--- src/app/layouts/full/sidenav2/sidenav.component.ts
+++ src/app/layouts/full/sidenav2/sidenav.component.ts
@@ -1,9 +1,8 @@
-import { animate, keyframes, style, transition, trigger } from '@angular/animations';
-import { Component, Output, EventEmitter, OnInit, HostListener } from '@angular/core';
-import { Router } from '@angular/router';
-import { fadeInOut, INavbarData } from './helper';
-import { navbarData } from './nav-data';
-import {HttpClient} from "@angular/common/http";
+import {animate, keyframes, style, transition, trigger} from '@angular/animations';
+import {Component, Output, EventEmitter, OnInit, HostListener} from '@angular/core';
+import {Router} from '@angular/router';
+import {fadeInOut, INavbarData} from './helper';
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {environment} from "../../../../environments/environment";
 
@@ -39,18 +38,29 @@ export class SidenavComponent implements OnInit {
   @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
   collapsed = true;
   screenWidth = 0;
-  navData : INavbarData[]=navbarData;
+  navData: INavbarData[];
   multiple: boolean = false;
+  subItem: INavbarData[];
 
-  constructor(public router: Router, private http:HttpClient) {
-    // http.get<INavbarData[]>(GENERATE_TRANSCRIPT_API + 'getMenu').subscribe((res)=>this.navData = res);
+  constructor(public router: Router, private http: HttpClient) {
+    http.get<INavbarData[]>(GENERATE_TRANSCRIPT_API + 'getMenu').subscribe((res) => this.navData = res);
   }
+
   ngOnInit(): void {
 
   }
 
   handleClick(item: INavbarData): void {
-    this.shrinkItems(item);
+
+    let queryParams:HttpParams = new HttpParams();
+
+    queryParams = queryParams.append("rootId", item.id);
+    this.http.get<INavbarData[]>(GENERATE_TRANSCRIPT_API + 'get_menu_by_root_id', {params: queryParams}).subscribe((res) => {
+      this.subItem = res
+      console.log(this.subItem);
+      this.shrinkItems(this.subItem);
+    });
+
     item.expanded = !item.expanded
   }
 
@@ -58,12 +68,10 @@ export class SidenavComponent implements OnInit {
     return this.router.url.includes(data.routeLink) ? 'active' : '';
   }
 
-  shrinkItems(item: INavbarData): void {
+  shrinkItems(item: INavbarData[]): void {
     if (!this.multiple) {
-      for(let modelItem of this.navData) {
-        if (item !== modelItem && modelItem.expanded) {
-          modelItem.expanded = false;
-        }
+      for (let modelItem of item) {
+        modelItem.expanded = false;
       }
     }
   }
