fe363fea2e7fa7635ded5ad37e1b5733aff7abf0
Janith <wajwi@ou.ac.lk>
2023-06-12 20:07:51 +0530

    icon

--- src/app/shared/services/permission.service.ts
+++ src/app/shared/services/permission.service.ts
@@ -9,13 +9,22 @@ const GENERATE_TRANSCRIPT_API = environment.base_url_user_mgt + '/api/user_manag
   providedIn: 'root'
 })
 export class PermissionService {
+  get permission(): any {
+    return this._permission;
+  }
+
+  set permission(value: any) {
+    this._permission = value;
+  }
 
   constructor(private http :HttpClient) { }
 
+  private _permission:any;
   getPermission(): Observable<any> {
     let result = this.http.get<INavbarData[]>(GENERATE_TRANSCRIPT_API + 'get_permissions', {params: new HttpParams().append("role_name", 'admin')});
     return new Observable(observable => {
       observable.next(result.toPromise().then((result: any) => {
+        this.permission = result;
         observable.next(result);
         observable.complete();
       })
