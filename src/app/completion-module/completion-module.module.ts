20e6fc87db7cb099a738d48acc2a1867679cb45c
hsdha <hsdha@ou.ac.lk>
2023-07-12 12:10:05 +0530

    completion management changes 2023.07.12

--- src/app/completion-module/completion-module.module.ts
+++ src/app/completion-module/completion-module.module.ts
@@ -2,6 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CompletionModuleComponent } from './generate-result/completion-module/completion-module.component';
 import {CompletionModuleRoutingModule} from "./completion-module-routing.module";
+import {DataTablesModule} from "angular-datatables";
+import {FormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatOptionModule} from "@angular/material/core";
+import {MatSelectModule} from "@angular/material/select";
+import {NgxMatSelectSearchModule} from "ngx-mat-select-search";
 
 
 
@@ -11,7 +22,18 @@ import {CompletionModuleRoutingModule} from "./completion-module-routing.module"
   ],
   imports: [
     CommonModule,
-    CompletionModuleRoutingModule
+    CompletionModuleRoutingModule,
+    DataTablesModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatOptionModule,
+    MatSelectModule,
+    NgxMatSelectSearchModule
   ]
 })
 export class CompletionModuleModule { }
